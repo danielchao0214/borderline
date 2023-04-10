ad6db9574c351f8d638b1cf8f0a784c88b08f862
Scott Imandt <scottimandt@gmail.com>
2023-04-10 14:14:19 -0400

    Working on AppBanner

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import AppBanner from '@/components/AppBanner.js'
 import '@/styles/globals.css'
+import '@/styles/AppBanner.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <>
+      <AppBanner/>
+      <Component {...pageProps} />
+    </>
+  )
 }
