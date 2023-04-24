a44a820f7adb5a7a444e6e983bfb485360f95562
Scott Imandt <scottimandt@gmail.com>
2023-04-24 13:55:16 -0400

    Removed <a> and commented out darkmode css

--- src/components/ForumPostCard.js
+++ src/components/ForumPostCard.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react'
 import styles from '@/styles/ForumPostCard.module.css'
 
-
 function ForumPostCard({ post }) {
     return (
         <>
             <div className={styles.forumdiv}>
                 {/* temp link need  to change */}
-                <a href='/forumview'>
+                
                     <h1>{post.title}</h1>
                     <p>Posted by: {post.user}</p>
                     <br></br>
                     <p>{post.message}</p>
-                </a>
+                
             </div>
         </>
     )
