2b240cc53eb5a4291fe614867c9682eeb6a84440
Scott Imandt <scottimandt@gmail.com>
2023-04-24 13:35:58 -0400

    Created React components for both forum and recently viewd forum and fixed css formatting for page

---
+++ src/components/ForumPostCard.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import styles from '@/styles/ForumPostCard.module.css'
+
+
+function ForumPostCard({ post }) {
+    return (
+        <>
+            <div className={styles.forumdiv}>
+                {/* temp link need  to change */}
+                <a href='/forumview'>
+                    <h1>{post.title}</h1>
+                    <p>Posted by: {post.user}</p>
+                    <br></br>
+                    <p>{post.message}</p>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default ForumPostCard
