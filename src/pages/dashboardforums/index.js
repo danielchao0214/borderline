8dd3a8ab9883d7c88cae46b19283321a003ca53e
Scott Imandt <scottimandt@gmail.com>
2023-05-01 14:00:34 -0400

    Added no filter Forum post search using similar based searching methods (Strigger is sill create forum post)

--- src/pages/dashboardforums/index.js
+++ src/pages/dashboardforums/index.js
@@ -1,46 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import styles from '@/pages/dashboardforums/DashboardForums.module.css'
 import ForumPostList from '@/components/ForumPostList';
 import RecentPostList from '@/components/RecentPostList';
 import Button from '@mui/material/Button';
+import { AppBannerContext } from '@/components/contexts/AppBannerContext';
 
 export default function DashboardForums() {
 
   // Data loaded in these useStates is testing data and should be empty at start and use
   // IE  const [postList, setPost] = useState([]);
-  const [postList, setPost] = useState([{ id: 1, title: "Title1", user: "user1", message: "message" }]);
+  const [postList, setPost] = useState([{}]);
   const [recentPostList, setRecentPostList] = useState([{ id: 2, title: "title", user: "user" }]);
+  const {value, setValue} = React.useContext(AppBannerContext)
+  
+  useEffect(() => {
+    // inital fire of getForumPost
+    console.log(value)
+  }, [value]);
 
-  //Temporay fucnction that should called when search results are provided
-  function temporaryOnClick (event){
-    setPost(prevPostList => {
-      return [...prevPostList, { id: 1, title: "Title1", user: "user1", message: "message" }]
-    })
-  }
+  const getForumPost = async (event) => {
+    const search = {value} // this will be the search in text field
+    event.preventDefault();
+    let url = "/api/getForumPost"
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        search,
+      }),
+      headers: {
+        "content-type": "application/json"
+      },
+    }).catch((e) => console.log(e));
+
+    // wait for the responce from request and get the body
+    const data = await res.json();
+
+    // If status code returns error print the code in the body
+    if (res.status == 401) {
+      console.log(data.errorMessage);
+    }
+
+    //If route is good then log the results
+    if (res.status == 200) {
+      console.log(data.forumPosts)
+      console.log(data.message)
+
+      //Change state !!!!!!!!
+      setPost(data.forumPosts)
+
+    }
+  };
 
   return (
     <>
-    <div className={styles.mainContainer}>
-      <div className={styles.createcontainer}>
-        <div className = {styles.createButtonContainter}>
-        {
-          <Button className={styles.createbutton} variant="outlined" onClick={temporaryOnClick}>Create Post</Button>
-        }
+      <div className={styles.mainContainer}>
+        <div className={styles.createcontainer}>
+          <div className={styles.createButtonContainter}>
+            {
+              <Button className={styles.createbutton} variant="outlined" onClick={getForumPost}>Create Post</Button>
+            }
+          </div>
+          <div className={styles.recentforumcontainer}>
+            <h1 className={styles.recentforumtitle} >Recently Viewed</h1>
+            <div className={styles.flexcontainer}>
+              <RecentPostList recentPostList={recentPostList} />
+            </div>
+          </div>
         </div>
-        <div className={styles.recentforumcontainer}>
-          <h1 className={styles.recentforumtitle} >Recently Viewed</h1>
-          <div className={styles.flexcontainer}>
-            <RecentPostList recentPostList={recentPostList} />
+
+        <div className={styles.forumcontainer}>
+          <div className={'styles.forumflexcontainer'}>
+            <ForumPostList postList={postList} />
           </div>
         </div>
       </div>
-
-      <div className={styles.forumcontainer}>
-        <div className={'styles.forumflexcontainer'}>
-          <ForumPostList postList={postList} />
-        </div>
-      </div>
-    </div> 
     </>
   )
 }
